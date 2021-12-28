b473ff8f2df69c1c0051c470fa4e1ceee184e701
jiebao <908822571@qq.com>
2021-12-28 20:11:34 +0800

    练习axios基础配置

--- src/App.vue
+++ src/App.vue
@@ -9,25 +9,40 @@
       <button @click="ChangeWidth" >修改大小</button>
       <button @click="ChangeOptions">修改样式</button>
     </div>
+    <hr>
+    <span style="display: flex ; font-size: 50px">练习axios</span>
+    <div style="display: flex ;margin-top: 30px">
+    <LeanAxios color="pink" @click="Get">get请求</LeanAxios>
+      <LeanAxios color="purple" @click="Post">post请求</LeanAxios>
+      <LeanAxios color="yellow" @click="Put">put请求</LeanAxios>
+      <LeanAxios color="pink"  @click="Delete">delete请求</LeanAxios>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import MyEcharts from "@/components/MyEcharts";
+import LeanAxios from "@/components/LeanAxios";
 import ElInput from "@/components/ElInput";
 import {options1,options2} from "@/components/options";
+axios.defaults.method='GET'
+axios.defaults.baseURL='http://localhost:3000'
+axios.defaults.params={id:100}
+
 export default {
   name: 'App',
   components: {
     ElInput,
-    MyEcharts
+    MyEcharts,
+    LeanAxios
   },
   data(){
     return{
       name:'',
       options: options1,
       width:'600px',
-      height:'400px'
+      height:'400px',
     }
   },
   watch:{
@@ -36,6 +51,38 @@ export default {
     }
   },
   methods:{
+    Get(){
+      axios({
+        url:'/posts'
+      }).then(
+          res=>{
+            console.log(res)
+          }
+      )
+    },
+    Post(){
+      axios({
+        methods: "POST",
+        url:'http://localhost:3000/posts',
+        data:{
+          "title": "廖玉洁真可爱",
+          "author": "玉洁"
+        }
+      }).then(res=>console.log(res.data))
+    },
+    Put(){
+      axios({
+        methods: "PUT",
+        url:'http://localhost:3000/posts/1',
+        data:{
+          "title": "廖玉洁真可爱",
+          "author": "玉洁"
+        }
+      }).then(res=>console.log(res.data))
+    },
+    Delete(){
+
+    },
     change(){
       console.log('focus实现了一次')
     },
@@ -55,7 +102,9 @@ export default {
       }else{
         this.options = options1
       }
-    }
+    },
+
+
     }
 }
 </script>

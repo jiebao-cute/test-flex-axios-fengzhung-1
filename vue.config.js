93608c1e96e899fc74c46737b86a1ce7c1132fb0
jiebao <908822571@qq.com>
2021-12-28 20:11:12 +0800

    解决跨域

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,15 @@ module.export ={
     publicPath: process.env.NODE_ENV === 'production'
         ? './'
         : '/',
-    outputDir:process.env.outputDir
+    outputDir:'dist',
+    assetsDir:'assets',//放置静态文件夹目录
+    proxyTable: {//跨域
+        '/api': {
+            changeOrigin: true,// 如果接口跨域，需要进行这个参数配置
+            target: 'http://localhost:3000',// 接口的域名
+            pathRewrite: {
+                '^/api': ''//后面可以使重写的新路径，一般不做更改
+            }
+        }
+    },
 }
